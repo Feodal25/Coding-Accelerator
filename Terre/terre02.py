af9b1d7eb63f82f9a4240f1a4eedb8c20c22eb25
Gaétan <gaetan@joyeux.ch>
2026-03-10 12:20:37 +0100

    j'avais oublié de save mon code pour le dernier commit. voici donc le code simplifié grâce à une for loop

--- Terre/terre02.py
+++ Terre/terre02.py
@@ -1,10 +1,5 @@
 import sys
 args_without_filename = sys.argv[1:]   
 
-# index = 0
-# while index < len(args_without_filename):
-#     print(args_without_filename[index])
-#     index += 1
-
 for arg in args_without_filename:
     print(arg)
