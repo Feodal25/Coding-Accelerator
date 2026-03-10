a12cb0714b0e61486a6bb87fdbbbc4142764cf8f
Gaétan <gaetan@joyeux.ch>
2026-03-10 11:06:05 +0100

    meilleur nom variable. Version finale après retour Romain F.

--- Terre/terre00.py
+++ Terre/terre00.py
@@ -1,21 +1,8 @@
-first_letter = 97
-current_letter = chr(first_letter)
-count = 0
-
-while count < 26:
-    print(current_letter, end='')
-    first_letter += 1
-    count += 1
-    current_letter = chr(first_letter)
-
-print()
-
-#---------
-# Deuxième version : en utilisant une boucle for au lieu de while
-
 alphabet = ""
+a_ascii_value = 97
+z_ascii_value = 123
 
-for i in range(97, 123): 
+for i in range(a_ascii_value, z_ascii_value): 
     alphabet = alphabet + chr(i)
 
 print(alphabet)
