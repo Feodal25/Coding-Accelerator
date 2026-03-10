0564e794a306f8e79e223de5956d643de289e59d
Gaétan <gaetan@joyeux.ch>
2026-03-10 11:30:27 +0100

    new version en utilisant module sys et propriété .argv

--- Terre/terre01.py
+++ Terre/terre01.py
@@ -1,6 +1,4 @@
-file_path = __file__
-all_slash_index = [i for i, slash in enumerate(file_path) if slash == "/"]
-slash_number = file_path.count("/")
-last_slash_index = (all_slash_index[slash_number - 1])
+import sys
+file_name = sys.argv[0]
 
-print(file_path[last_slash_index + 1:])
+print(file_name)
