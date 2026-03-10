fb7040ff7e1941471057e1f22374a4177e942eeb
Gaétan <gaetan@joyeux.ch>
2026-03-10 18:47:58 +0100

    Et voilaaaa

--- Terre/terre04.py
+++ Terre/terre04.py
@@ -7,6 +7,10 @@ if len(args) != 1:
 arg1 = args[0]
 
 if (arg1.isdecimal()) or (arg1[0] == "-" and arg1[1:].isdecimal()):
-    print("super on continue")
+    arg1 = int(arg1)
+    if arg1 % 2 == 0:
+        print("pair")
+    else:
+        print("impair")
 else:
     print("Tu ne me la mettras pas à l'envers")
