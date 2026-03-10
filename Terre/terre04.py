44976d87eae6eb19e0d00b6c10a6b5d48956cfcb
Gaétan <gaetan@joyeux.ch>
2026-03-10 18:16:27 +0100

    Solutionné pour quand y a + de 1 argument mais pas encore quand y a 0 argument

--- Terre/terre04.py
+++ Terre/terre04.py
@@ -2,7 +2,7 @@ import sys
 args = sys.argv[1:]
 arg1 = args[0]
 
-if (len(args) == 1 and arg1.isdecimal()) or (arg1[0] == "-" and arg1[1:].isdecimal()):
+if len(args) == 1 and ((arg1.isdecimal()) or (arg1[0] == "-" and arg1[1:].isdecimal())):
     print("super on continue")
 else:
     print("Tu ne me la mettras pas à l'envers")
