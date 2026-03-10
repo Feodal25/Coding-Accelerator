35f31ba8fc23dd5619df341ec8d4314b55f86aff
Gaétan <gaetan@joyeux.ch>
2026-03-10 12:19:05 +0100

    New version simplifiée avec boucle for

--- Terre/terre02.py
+++ Terre/terre02.py
@@ -1,7 +1,10 @@
 import sys
 args_without_filename = sys.argv[1:]   
 
-index = 0
-while index < len(args_without_filename):
-    print(args_without_filename[index])
-    index += 1
+# index = 0
+# while index < len(args_without_filename):
+#     print(args_without_filename[index])
+#     index += 1
+
+for arg in args_without_filename:
+    print(arg)
