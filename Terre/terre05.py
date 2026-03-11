00731b33567235ed3612a570892dadc1fe64abb7
Gaétan <gaetan@joyeux.ch>
2026-03-11 15:45:35 +0100

    Version fonctionnelle. Erreurs couverte après test

--- Terre/terre05.py
+++ Terre/terre05.py
@@ -1,15 +1,3 @@
-arg1 = 11
-arg2 = 4
-
-résultat = arg1 / arg2
-résultat = int(résultat)
-print(f"résultat : {résultat}")
-
-reste = arg1 % arg2
-print(f"reste : {reste}")
-
-
-#--------------
 import sys
 args = sys.argv[1:]
 if len(args) != 2:
@@ -19,11 +7,12 @@ if len(args) != 2:
 arg1 = args[0]
 arg2 = args[1]
 
-if (arg1.isdecimal()) or (arg1[0] == "-" and arg1[1:].isdecimal()):
+if (arg1.isdecimal() and arg2.isdecimal() and (arg1 >= arg2)) or ((arg1[0] == "-" and arg1[1:].isdecimal()) and (arg2[0] == "-" and arg2[1:].isdecimal()) and (arg1 >= arg2)):
 
     arg1 = int(arg1)
     arg2 = int(arg2)
-    résultat = arg1 / arg2
+    résultat = arg1 // arg2                                 # "//" = division entière, retourne toujours un int (arrondi vers le bas)
+    résultat = int(résultat)
     reste = arg1 % arg2
 
     print(f"résultat : {résultat}")
