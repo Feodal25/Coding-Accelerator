2f8d94d7cbe468d54e07a0199c8b2e345513aef6
Gaétan <gaetan@joyeux.ch>
2026-03-11 16:53:13 +0100

    Version simplifié au max avec definitions de fonctions carrément

--- Terre/terre05.py
+++ Terre/terre05.py
@@ -1,20 +1,29 @@
 import sys
-args = sys.argv[1:]
-if len(args) != 2:
+
+def message_erreur():
     print("erreur.")
     sys.exit()
+def est_valide(arg):
+    return arg.isdecimal() or (arg[0] == "-" and arg[1:].isdecimal())
+
+args = sys.argv[1:]
+
+if len(args) != 2:
+    message_erreur()
 
 arg1 = args[0]
 arg2 = args[1]
 
-if (arg1.isdecimal() and arg2.isdecimal() and (arg1 >= arg2)) or ((arg1[0] == "-" and arg1[1:].isdecimal()) and (arg2[0] == "-" and arg2[1:].isdecimal()) and (arg1 >= arg2)):
+if (not est_valide(arg1) or not est_valide(arg2)):
+    message_erreur()
 
-    arg1 = int(arg1)
-    arg2 = int(arg2)
-    résultat = arg1 // arg2                                 # "//" = division entière, retourne toujours un int (arrondi vers le bas)
+arg1 = int(arg1)
+arg2 = int(arg2)
+
+if (arg1 < arg2) or (arg2 == 0):
+    message_erreur()
+else:
+    résultat = arg1 // arg2
     reste = arg1 % arg2
-
     print(f"résultat : {résultat}")
     print(f"reste : {reste}")
-else:
-    print("erreur.")
