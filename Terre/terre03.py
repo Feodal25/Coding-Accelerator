29a95cded369f04a99e300825abce63644f9a8b2
Gaétan <gaetan@joyeux.ch>
2026-03-14 12:21:45 +0100

    simplification et variables renommée

--- Terre/terre03.py
+++ Terre/terre03.py
@@ -1,22 +1,19 @@
 import sys
 arguments = sys.argv[1:]
 
-if len(arguments) != 1:
-	print("argument number incorrect")
+if len(arguments) != 1 or len(arguments[0]) != 1:
+	print("argument number or size incorrect")
 	sys.exit()
 
-if len(arguments[0]) != 1:
-	print("argument size incorrect")
-	sys.exit()
-
+argument = arguments[0]
 first_letter = 97
 last_letter = 122
 
-if ord(arguments[0]) < first_letter or ord(arguments[0]) > last_letter:
+if ord(argument) < first_letter or ord(argument) > last_letter:
 	print("argument has to be a lowercase letter")
 	sys.exit()
 
-starting_letter = ord(arguments[0])
+starting_letter = ord(argument)
 result = ""
 
 for i in range(starting_letter, last_letter + 1):
