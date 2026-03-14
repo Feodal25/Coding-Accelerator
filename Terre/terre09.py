795e0321d2057c11c8f9a659731787240b6a2a33
Gaétan <gaetan@joyeux.ch>
2026-03-14 17:51:14 +0100

    Fonctionne pour les carrés parfait mais pour les autres, ça arrondi au dessus... faut que j'améliore ça

---
+++ Terre/terre09.py
@@ -0,0 +1,19 @@
+import sys
+arguments = sys.argv[1:]
+
+if len(arguments) != 1:
+    sys.exit("arguments number incorrect")
+
+if not arguments[0].isdigit():
+    sys.exit("argument must be a positif number")
+
+radicand = int(arguments[0])
+impairs = 1
+count = 0
+
+while radicand > 0:
+    radicand = radicand - impairs
+    impairs += 2
+    count += 1
+
+print(count)
