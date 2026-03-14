a3b37b35452905d121f22e090d09f73c2e445c9c
Gaétan <gaetan@joyeux.ch>
2026-03-14 17:12:54 +0100

    dernière modif variable name

--- Terre/terre07.py
+++ Terre/terre07.py
@@ -4,14 +4,13 @@ arguments = sys.argv[1:]
 if len(arguments) != 1:
     sys.exit("écris une chaîne de caractères")
 
-argument = arguments[0]
-
-if argument.isdigit():
+if arguments[0].isdigit():
     sys.exit("écris une chaîne de caractères")
 
+string = arguments[0]
 count = 0
 
-for caracters in argument:
+for character in string:
     count += 1
 
 print(count)
