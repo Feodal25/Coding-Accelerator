e79e25a4c0e7097ceb42fc034b23f450dca4da5e
Gaétan <gaetan@joyeux.ch>
2026-03-15 17:49:31 +0100

    Je viens récup mes variables hours minutes et period

--- Terre/terre12.py
+++ Terre/terre12.py
@@ -12,4 +12,25 @@ if len(time) != 7:
 if not time[:2].isdigit() or not time[3:5].isdigit() or time[2] != ":" or time[5:7] not in ["AM", "PM", "am", "pm"]:
     sys.exit("argument must be time in format 12h")
 
-print(time)
+hours = ""
+minutes = ""
+period = ""
+
+for char in time:
+    if char == ":":
+        break
+    else:
+        hours += char
+
+for char in time[3:5]:
+    minutes += char
+
+for char in time[5:7]:
+    period += char
+
+print(hours)
+print(minutes)
+print(period)
+
+hours = int(hours)
+minutes = int(minutes)
