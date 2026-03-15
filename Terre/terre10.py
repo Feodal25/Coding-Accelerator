414f2ab294c873878b028804e723d2d243506988
Gaétan <gaetan@joyeux.ch>
2026-03-15 16:14:20 +0100

    J'ai rajouté condition spécialement pour number = 0, 1 ou 2

--- Terre/terre10.py
+++ Terre/terre10.py
@@ -8,6 +8,15 @@ if not arguments[0].isdigit():
     sys.exit("argument must be a positif number")
 
 number = int(arguments[0])
+
+if number <= 1:                                                     # J'exclue 0 et 1 de mes scénarios car chiant pour formule après
+    print(f"Non, {number} n'est pas un nombre premier.")
+    sys.exit()
+
+if number == 2:
+    print(f"Oui, {number} est un nombre premier")                   # J'exclue également 2 de mes scénarios car divisor = 2
+    sys.exit()
+
 divisor = 2
 
 while number > divisor:
