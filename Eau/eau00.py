46965571ed85b0ff2d7c9f35acddfd4249259639
Gaétan <gaetan@joyeux.ch>
2026-03-20 13:52:21 +0100

    second version using a simopler algo

--- Eau/eau00.py
+++ Eau/eau00.py
@@ -1,32 +1,11 @@
-def add_leading_zeros(number):
-    chars = str(number)
-    while len(chars) < 3:
-        if len(chars) == 1:
-            chars = "00" + chars
-        chars = "0" + chars
-    return chars
+def get_three_digits_numbers():
+    combinations = []
+    for first_digit in range(0,10):
+        for second_digit in range(first_digit + 1, 10):
+            for third_digit in range(second_digit + 1, 10):
+                combinations.append(f"{first_digit}{second_digit}{third_digit}")
+    return combinations
 
-def has_distinct_digits(number):
-    chars = str(number)
+resultat = get_three_digits_numbers()
 
-    if chars[0] != chars[1] and chars[1] != chars[2] and chars[0] != chars[2]:
-        return True
-    return False
-
-def is_ascending(number):
-    chars = str(number)
-
-    if chars[0] < chars[1] and chars[1] < chars[2]:
-        return True
-    return False
-
-numbers = range(0, 1000)
-wanted_numbers = []
-resultat = ()
-
-for number in numbers:
-    if has_distinct_digits(add_leading_zeros(number)) and is_ascending(add_leading_zeros(number)):
-        wanted_numbers.append(add_leading_zeros(number))
-        resultat = (", ".join(wanted_numbers))
-
-print(resultat)
+print((", ".join(resultat)))
